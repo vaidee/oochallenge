3a519643a603f59e6ae7a0f4944bcc4d236d5f4f
vaideeswaran mathappan <www.vaidees@gmail.com>
2014-09-21 00:16:27 -0700

    initial version of java app

---
+++ src/test/java/com/ooyala/MyResourceTest.java
@@ -0,0 +1,29 @@
+//package com.interview;
+/*
+import javax.ws.rs.core.Application;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import com.interview.MyResource;
+*/
+//public class MyResourceTest extends JerseyTest {
+
+    /*@Override
+    protected Application configure() {
+        return new ResourceConfig(MyResource.class);
+    }*/
+
+    /**
+     * Test to see that the message "Got it!" is sent in the response.
+     */
+    /*@Test
+    public void testGetIt() {
+        final String responseMsg = target().path("myResource").request().get(String.class);
+
+        assertEquals("Hello, Candidate.. My name is Breakme :) Please write the client to test me.Please try breaking me!!", responseMsg);
+    }*/
+//}
